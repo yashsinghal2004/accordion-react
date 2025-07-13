04f6efe61cbc0cf61a1a5ec444476fb6abf58c73
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2025-07-13 17:26:09 +0530

    Completed accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
+import {FaChevronDown, FaChevronUp} from "react-icons/fa";
 
 const Accordion=({items})=>{
+    const [openItem,setOpenItem]= useState(null);
+
+    const handlecuritem=(index)=>{
+        setOpenItem(openItem==index ?null : index);
+    }
     return(
         <div className="accordion">
             {
                 items.map((item,index)=>{
                     return(
-                        <div key={index}>
-                            <p>{item.title}</p>
-                            <p>{item.content}</p>
+                        <div key={index} className="accordion-item">
+                            <button className="accordion-title" onClick={()=>handlecuritem(index)}>
+                                {item.title}
+                                <div className={"icon"}>{openItem==index ? <FaChevronUp/> : <FaChevronDown/>}</div>
+                                
+                                </button>
+                            {openItem==index && <p className="accordion-content">{item.content}</p>}
                         </div>
                     )
                 })
